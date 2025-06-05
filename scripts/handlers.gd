1499ab44d717b06fcd952f96de4df6ca42b12f25
Flloyd Nathaniel Gagni <fnbg720@gmail.com>
2025-06-05 21:58:45 +0800

    Add final touches and export.
    Signed-off-by: Flloyd Nathaniel Gagni <fnbg720@gmail.com>

--- scripts/handlers.gd
+++ scripts/handlers.gd
@@ -1,27 +1,14 @@
 extends Control
 
-@onready var top: Control = $Top
 @onready var right: Control = $Right
 @onready var bottom: Control = $Bottom
-@onready var left: Control = $Left
-
-@onready var upper_left_corner: Control = $UpperLeftCorner
-@onready var upper_right_corner: Control = $UpperRightCorner
 @onready var bottom_right_corner: Control = $BottomRightCorner
-@onready var bottom_left_corner: Control = $BottomLeftCorner
 
 var resizing : bool = false
 var resize_node : Control
-var initial_window_pos: Vector2i
-var initial_window_size: Vector2i
-var initial_mouse_pos: Vector2
 
 func _ready() -> void:
-	var handles : Array[Control] = [
-		top, bottom, right, left, 
-		upper_left_corner, upper_right_corner, 
-		bottom_right_corner, bottom_left_corner
-	]
+	var handles : Array[Control] = [bottom, right, bottom_right_corner]
 	
 	for handle in handles:
 		handle.gui_input.connect(
@@ -36,22 +23,7 @@ func _gui_input_handling(node : Control):
 
 func _process(_delta: float) -> void:
 	if resizing:
-		#if resize_node in [top, upper_left_corner]:
-			#get_window().size.y = int(get_global_mouse_position().y)
-		#if resize_node in [left, upper_left_corner]:
-			#get_window().size.x = int(get_global_mouse_position().x)
-		#
-		#if resize_node in [top, upper_right_corner]:
-			#get_window().size.y = int(get_global_mouse_position().y)
-		#if resize_node in [right, upper_right_corner]:
-			#get_window().size.x = int(get_global_mouse_position().x)
-		
 		if resize_node in [bottom, bottom_right_corner]:
 			get_window().size.y = int(get_global_mouse_position().y)
 		if resize_node in [right, bottom_right_corner]:
-			get_window().size.x = int(get_global_mouse_position().x)
-		
-		if resize_node in [bottom, bottom_left_corner]:
-			get_window().size.y = int(get_global_mouse_position().y)
-		if resize_node in [left, bottom_left_corner]:
 			get_window().size.x = int(get_global_mouse_position().x)
